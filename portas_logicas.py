0143f544580d18fe773d4edba53bf26d55b04ba4
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-09-30 16:56:26 +0000

    exemplos de portas logicas

---
+++ portas_logicas.py
@@ -0,0 +1,17 @@
+#são usadas oara lidar com booleanos 
+
+# bool1 = True
+# bool2 = False 
+
+# print(bool1 and bool2) 
+
+var1 = 5
+var2 = 10
+var3 = 2
+
+#para dar True, tudo tem que ser verdadeiro, ao passo que para dar falso, uma das opções deve ser falsa ou todas devem ser falsas 
+print(var1 > var2) 
+print(var1 == var3)
+print(var2 > var1 > var3)
+print(var1 < var2 < var3)
+
