afd63c7530942205cf31c11a1ba3002156a29da8
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-01 14:38:50 +0000

    formatando a questão 2

--- Questão_02.py
+++ Questão_02.py
@@ -5,10 +5,10 @@
 import math
 
 #usar o while True para somar infinitamente 
-n = 1
+n = 2
 while True:
-    n = n + 1 
     termo = 1/n*(math.log(n,math.e))**2 #1/n*ln^2(n)
+    n = n + 1 
     termo_arredondado = round(termo,7) #diminuindo o numero de casas decimais para meu pc não explodir
     print(termo_arredondado)
 
