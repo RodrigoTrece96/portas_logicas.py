333099c1b3b8548356fd782cfb61f799146f6825
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-02 12:44:45 +0000

    corrigindo a formula

--- Questão_02.py
+++ Questão_02.py
@@ -7,7 +7,7 @@ import math
 #usar o while True para somar infinitamente 
 n = 2
 while True:
-    termo = 1/n*(math.log(n,math.e))**2 #1/n*ln^2(n)
+    termo = 1/(n*(math.log(n,math.e))**2) #1/n*ln^2(n)
     n = n + 1 
     termo_arredondado = round(termo,7) #diminuindo o numero de casas decimais para meu pc não explodir
     print(termo_arredondado)
