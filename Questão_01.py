68da52aad198a4af8489318353e90eb0c8f72a0f
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-04 22:09:13 +0000

    Finalizando a questao 1

--- Questão_01.py
+++ Questão_01.py
@@ -1,23 +1,41 @@
-#criando um pedra papel tesoura no python 
+import random 
 
-import random
+possibilidades = ('pedra', 'papel', 'tesoura')
+rodadas = 0
+score_usuario = 0
+score_computador = 0
 
-#criando uma tupla com as possibilidades 
+while True:
+#
+# Definindo o número de rodadas
+    if score_usuario == 3 or score_computador == 3:
+        print('A partida acabou!')
+        break 
+# Defindindo as jogadas
+    jogada_do_computador = random.choice(possibilidades)
+    jogada_usuário = input('Escolha pedra, papel ou tesoura: ')
 
-possibilidades = ('pedra','papel','tesoura')
-primeira_jogada = input('Digite sua jogada: ')
-var1 = random.choice(possibilidades)
 
-pontuacao_computador = 0
-minha_pontuacao = 0 
+# Casos inválidos
+    if jogada_usuário != 'pedra' or 'papel' or 'tesoura':
+        print('Jogada inválida')
+        continue
 
-if primeira_jogada == var1:
-    print('empate')
-elif primeira_jogada == 'tesoura' and var1 == 'pedra':
+# Caso em que o jogo empata
+    if jogada_usuário == jogada_do_computador:
+        print('Empate')
+        continue 
+
+#Caso em que o computador vence
+    if (jogada_do_computador == 'pedra' and jogada_usuário == 'tesoura') or \
+        (jogada_do_computador == 'papel' and jogada_usuário == 'pedra') or \
+        (jogada_do_computador == 'tesoura' and jogada_usuário == 'papel'):
+        print('O computador venceu!')
+        rodadas += 1 
+        score_computador += 1 
     
-    print('o computador ganhou')
-elif 
+#Caso em que o usuário vence
+    if (jogada_usuário)
 
 
-
-     
+    
