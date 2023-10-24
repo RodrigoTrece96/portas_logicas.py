3bf92c7a97a8a256fb9073789a59b3b6871320dc
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-24 20:56:26 +0000

    Finalmente o pedra papel tesoura!

--- Questão_01.py
+++ Questão_01.py
@@ -6,36 +6,45 @@ score_usuario = 0
 score_computador = 0
 
 while True:
-#
+
+# Mostrando o placar a cada jogada
+    print(f"Pontuação: {score_usuario}:{score_computador}") #formatando com f-strings
+    print('Rodada: ', rodadas)
+
 # Definindo o número de rodadas
     if score_usuario == 3 or score_computador == 3:
         print('A partida acabou!')
         break 
+
 # Defindindo as jogadas
     jogada_do_computador = random.choice(possibilidades)
-    jogada_usuário = input('Escolha pedra, papel ou tesoura: ')
-
+    jogada_usuario = input('Escolha pedra, papel ou tesoura: ')
 
 # Casos inválidos
-    if jogada_usuário != 'pedra' or 'papel' or 'tesoura':
+    if jogada_usuario not in possibilidades:
         print('Jogada inválida')
         continue
 
 # Caso em que o jogo empata
-    if jogada_usuário == jogada_do_computador:
+    if jogada_usuario == jogada_do_computador:
         print('Empate')
+        rodadas += 1 
         continue 
 
 #Caso em que o computador vence
-    if (jogada_do_computador == 'pedra' and jogada_usuário == 'tesoura') or \
-        (jogada_do_computador == 'papel' and jogada_usuário == 'pedra') or \
-        (jogada_do_computador == 'tesoura' and jogada_usuário == 'papel'):
+    if (jogada_do_computador == 'pedra' and jogada_usuario == 'tesoura') or \
+        (jogada_do_computador == 'papel' and jogada_usuario == 'pedra') or \
+        (jogada_do_computador == 'tesoura' and jogada_usuario == 'papel'):
         print('O computador venceu!')
         rodadas += 1 
         score_computador += 1 
     
 #Caso em que o usuário vence
-    if (jogada_usuário)
+    if jogada_usuario == ('pedra' and jogada_do_computador == 'tesoura') or \
+        (jogada_usuario == 'papel' and jogada_do_computador == 'pedra') or \
+        (jogada_usuario == 'tesoura' and jogada_do_computador == 'papel'):
+        print('Você venceu!')
+        rodadas += 1 
+        score_usuario += 1 
+        continue 
 
-
-    
